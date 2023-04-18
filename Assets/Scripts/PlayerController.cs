eeef183de7dd8052722a7deea3f2926b0b0e3733
Ozan uysal <uysalozan92@gmail.com>
2023-04-19 01:54:35 +0300

    8

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,35 +6,59 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    Rigidbody2D rg2D;
+    [SerializeField] Rigidbody2D rb2D;
     PlayerController instance;
     bool _isGrounded;
+    [SerializeField] GameObject Onur;
+    bool doubleJump;
+    //Vector2 playerVelocity;
 
     [SerializeField] float torque = 1f;
     [SerializeField] int forceAmountY = 1; 
     void Start()
     {
-        rg2D = GetComponent<Rigidbody2D>();
         instance = this;
         _isGrounded = false;
+        //playerVelocity =  instance.GetComponent<Rigidbody2D>().velocity;
     }
     void Update()
     {
-        _isGrounded = GameObject.Find("GroundCheck").GetComponent<GroundCheck>().IsGrounded;
-       
-            
-        Debug.Log(_isGrounded);
+        //_isGrounded = GameObject.Find("GroundCheck").GetComponent<GroundCheck>().IsGrounded;
+        CharacterControll();
+    }
+    void CharacterControll()
+    {
+        RotateLeft();
+        RotateRight();
+        Jump();
+    }
+    void Jump()
+    {
+        
+        if (_isGrounded && !Input.GetButton("Jump"))
+        {
+            doubleJump = false;
+        }
+        _isGrounded = Onur.GetComponentInChildren<GroundCheck>().IsGrounded;
+        Debug.Log("DoubleJump = " + doubleJump);
+        if ((Input.GetButtonDown("Jump") && _isGrounded) || (Input.GetButtonDown("Jump") && doubleJump))
+        {
+            rb2D.AddForce(new Vector2(0,forceAmountY));
+            doubleJump = !doubleJump;
+        }
+    }
+    void RotateRight()
+    {
+       if (Input.GetKey(KeyCode.D))
+        {
+            rb2D.AddTorque(-torque);
+        }
+    }
+    void RotateLeft()
+    {
         if (Input.GetKey(KeyCode.A))
         {
-            rg2D.AddTorque(torque);
-        }
-        else if(Input.GetKey(KeyCode.D)) 
-        {
-            rg2D.AddTorque(-torque);
-        }
-        if (Input.GetKeyDown(KeyCode.Space) && _isGrounded)
-        {
-            instance.GetComponent<Rigidbody2D>().AddForce(new Vector2Int(0, forceAmountY));
+            rb2D.AddTorque(torque);
         }
     }
 }
